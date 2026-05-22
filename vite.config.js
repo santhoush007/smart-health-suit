3729c69bc2a25848d972c6b74e6032af7aa8789d
Santhoush S <santhoushs007@gmail.com>
2026-05-22 23:09:09 +0530

    Final deployed Smart Health Suit project

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ export default defineConfig({
       external: ['fsevents']
     }
   },
-  base: '/An-Intelligent-Smart-Health-Suit-Using-Embedded-Biosensors-for-Real-Time-Vital-Analysis/'
+  base: '/An-Intelligent-Smart-Health-Suit-Using-Embedded-Biosensors-for-Real-Time-Vital-Analysis/',
 })
